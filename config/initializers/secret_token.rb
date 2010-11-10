183ad903cadac169aaff86b979f868e4470f5d96
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2010-11-10 11:11:46 -0600

    Creando un esqueleto y agregados todos los archivos a git REAHACIENDO TUTORIAL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coderumble::Application.config.secret_token = 'b0193d0056865cf56090a5c4a290870b0be11dd524976cdf4b1de11eba97c2f5a951f53ff24d4ce1ffff290a8f76c46b120f7b6e46ee2576524c79580da42e75'
